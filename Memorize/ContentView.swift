b228d56dd5cd82fdb419f5daa0e653f84ad1bc05
maarlesh <subamaarleshbaskar@gmail.com>
2025-06-22 17:35:47 +0530

    Created LazyVGrid, Scroll View and Add and remove cards functionality

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,45 +1,62 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by EI-LAP-8464 on 19/06/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    let emojis : Array<String> = [
+        "😂","😳", "😴", "😂", "😉", "💁🏻", "🥰", "😇", "🤩", "😡",
+    ];
+    @State var cardCount : Int = 4;
     var body: some View {
-        HStack{
-            CardView()
-            CardView(isTurned: true)
-            CardView()
-            CardView()
-        }.padding(12)
+        VStack{
+            ScrollView{
+                LazyVGrid(columns: [
+                    GridItem(),
+                    GridItem(),
+    //                GridItem(), GridItem(), GridItem()
+                ]){
+                    ForEach(0..<cardCount, id: \.self) { index in
+                        CardView(content: emojis[index])
+                    }.aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+            Spacer()
+            HStack{
+                cardModifier(by: -1, label: "rectangle.stack.badge.minus.fill")
+                Spacer()
+                cardModifier(by: +1, label: "rectangle.stack.badge.plus.fill")
+            }.imageScale(.large).font(.largeTitle)
+        }.padding()
+    }
+    
+    func cardModifier(by offset : Int, label : String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }) {
+            Image(systemName: label)
+        }.disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
     }
 }
 
 
 struct CardView: View{
+    var content : String;
     @State var isTurned: Bool = false;
     
     var body: some View {
         ZStack (content: {
             let base  = RoundedRectangle(cornerRadius: 12);
-            if isTurned {
+            Group{
                 base
                     .foregroundColor(.white)
                 base
                     .strokeBorder(lineWidth: 2)
                     .foregroundColor(.cyan)
-                Text("❤️").font(.largeTitle)
-            }else{
-                base.foregroundColor(.cyan)
+                Text(content).font(.largeTitle)
             }
+            base.foregroundColor(.cyan).opacity(isTurned ? 0 : 1)
         }
         ).onTapGesture {
             isTurned.toggle()
         }
-        .frame(height: 200)
     }
 }
 
